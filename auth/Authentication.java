07e25c53a608579efdd94095060e6a05f483578a
harsheeen <harsheenkaur023@gmail.com>
2026-03-02 00:00:02 +0530

    Update Authentication.java

--- auth/Authentication.java
+++ auth/Authentication.java
@@ -1,7 +1,14 @@
+
+/*PROJECT - MYCONTACTAPP
+@Author Harsheen
+@Version 2.0 - Authentication Interface, implemented by Basic Authentication */
+
 package com.seveneleven.mycontactsapp.auth;
+
+import java.util.Optional;
 
 import com.seveneleven.mycontactsapp.user.model.User;
 
 public interface Authentication {
-	User login(String email, String password);
+    Optional<String> login(String username, String password);
 }
